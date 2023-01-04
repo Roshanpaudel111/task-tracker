980243d791c0a280b1006417af449c92e20588ec
Roshan Paudel <rozanpoudel@gmail.com>
2023-01-04 08:22:24 +0545

    Added Task and Tasks component with delete button on task and defined delete function to delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./index.css";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Sunday",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Go to the grocery store",
+      day: "Monday",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Biking",
+      day: "Friday",
+      reminder: true,
+    },
+  ]);
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,14 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Sunday",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Go to the grocery store",
-    day: "Monday",
-    reminder: False,
-  },
-  {
-    id: 3,
-    text: "Biking",
-    day: "Friday",
-    reminder: True,
-  },
-];
-const Tasks = () => {
-  return <div></div>;
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <>
+          <Task key={task.id} task={task} onDelete={onDelete} />
+        </>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
